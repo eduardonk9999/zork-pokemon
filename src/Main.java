183b7430ce5a64bd36458b400f8f48b0f6a19144
eduardonk9999 <thiago.novak7@gmail.com>
2023-04-02 14:35:53 -0300

    refactor: escolha do Pokemon

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,32 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Pokemon squirtle = new Squirtle("squirtle", "agua", "Jato D Agua");
-        squirtle.renderPokemon();
+        int numberPokemon = 0;
 
-        Pokemon bulbasaur = new Bulbasaur("Bulbasaur", "Planta", "Folhas Navalha");
-        bulbasaur.renderPokemon();
+        System.out.println("Qual Pokemon voce quer escolher?");
+        System.out.println("1 Squirtle, 2 Bulbasaur, 3 Charmander");
 
-        Pokemon charmander = new Charmander("Charmander", "Fogo", "Presas de Fogo");
-        charmander.renderPokemon();
+        Scanner scanner = new Scanner(System.in);
+        numberPokemon = scanner.nextInt();
+
+        int pokemonChosen = numberPokemon;
+
+        switch (pokemonChosen) {
+            case 1:
+                Pokemon squirtle = new Squirtle("squirtle", "agua", "Jato D Agua");
+                squirtle.renderPokemon();
+                break;
+            case 2:
+                Pokemon bulbasaur = new Bulbasaur("Bulbasaur", "Planta", "Folhas Navalha");
+                bulbasaur.renderPokemon();
+                break;
+            case 3:
+                Pokemon charmander = new Charmander("Charmander", "Fogo", "Presas de Fogo");
+                charmander.renderPokemon();
+                break;
+            default:
+                System.out.println("Escolha errada...");
+        }
+
     }
 }
