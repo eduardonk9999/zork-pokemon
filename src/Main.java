338f9a34111b363173a38331cc266893bcb70a85
eduardonk9999 <thiago.novak7@gmail.com>
2023-04-02 13:22:39 -0300

    feat: criando pokemons e renderizando eles

--- src/Main.java
+++ src/Main.java
@@ -2,43 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String yourPokemon = "";
-        int numberPokemon = 0;
+        Pokemon squirtle = new Squirtle("squirtle", "agua", "Jato D Agua");
+        squirtle.renderPokemon();
 
+        Pokemon bulbasaur = new Bulbasaur("Bulbasaur", "Planta", "Folhas Navalha");
+        bulbasaur.renderPokemon();
 
-        System.out.println("Qual Pokemon voce quer escolher?");
-        System.out.println("1 Squirtle, 2 Bulbasaur, 3 Charmander");
-
-
-        Scanner scanner = new Scanner(System.in);
-        numberPokemon = scanner.nextInt();
-
-
-        if(numberPokemon == 1){
-            Pokemon agua = new Pokemon();
-            agua.name = "Squirtle";
-            yourPokemon = agua.name;
-            agua.ataque = "Jato D'Agua";
-            agua.type = "Agua";
-            agua.renderPokemon();
-        } else if (numberPokemon == 2) {
-            Pokemon planta = new Pokemon();
-            planta.name = "Bulbasaur";
-            yourPokemon = planta.name;
-            planta.ataque = "Folhas Navalha";
-            planta.type = "Planta";
-            planta.renderPokemon();
-        } else {
-            Pokemon fogo = new Pokemon();
-            fogo.name = "Charmander";
-            yourPokemon = fogo.name;
-            fogo.ataque = "Presas de Fogo";
-            fogo.type = "Fogo";
-            fogo.renderPokemon();
-        };
-
-        Adventure aventura = new Adventure();
-        aventura.initAdventure(yourPokemon);
-
+        Pokemon charmander = new Charmander("Charmander", "Fogo", "Presas de Fogo");
+        charmander.renderPokemon();
     }
 }
--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,12 +1,18 @@
 public class Pokemon {
     String name;
     String type;
-    String ataque;
+    String attack;
+
+    Pokemon(String name, String type, String attack) {
+        this.name = name;
+        this.type = type;
+        this.attack = attack;
+    }
 
     void renderPokemon() {
         System.out.println("Seu Pokemon e " + name);
         System.out.println("Ele e do tipo: " + type);
-        System.out.println("O ataque dele e: " + ataque);
+        System.out.println("O ataque dele e: " + attack);
     }
 
 }
