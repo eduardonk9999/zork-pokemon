aeb6fbb5037888d3351bd52d1d7644300d430e09
eduardonk9999 <thiago.novak7@gmail.com>
2023-04-03 13:36:34 -0300

    feature: iniciando aventura

--- src/Adventure.java
+++ src/Adventure.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 public class Adventure {
-    String seuPokemon = "";
+    String currentPokemon = "";
     String response = "";
     void initAdventure (String yourPokemon) {
         System.out.println("Voce encontra um Pidgey o que voce faz?");
 
         Scanner scanner = new Scanner(System.in);
         response = scanner.nextLine();
-        seuPokemon = yourPokemon;
+        currentPokemon = yourPokemon;
 
-        if(response == seuPokemon) {
+        if(response == currentPokemon) {
             System.out.println("Inicia batalha POKEMON!!!");
         } else {
             System.out.println("Resposta errada!!!");
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int numberPokemon = 0;
+        String yourPokemon = "";
 
         System.out.println("Qual Pokemon voce quer escolher?");
         System.out.println("1 Squirtle, 2 Bulbasaur, 3 Charmander");
@@ -14,20 +15,25 @@ public class Main {
 
         switch (pokemonChosen) {
             case 1:
-                Pokemon squirtle = new Squirtle("squirtle", "agua", "Jato D Agua");
+                Pokemon squirtle = new Squirtle("Squirtle", "agua", "Jato D Agua");
+                yourPokemon = squirtle.name;
                 squirtle.renderPokemon();
                 break;
             case 2:
                 Pokemon bulbasaur = new Bulbasaur("Bulbasaur", "Planta", "Folhas Navalha");
+                yourPokemon = bulbasaur.name;
                 bulbasaur.renderPokemon();
                 break;
             case 3:
                 Pokemon charmander = new Charmander("Charmander", "Fogo", "Presas de Fogo");
+                yourPokemon = charmander.name;
                 charmander.renderPokemon();
                 break;
             default:
                 System.out.println("Escolha errada...");
         }
 
+        Adventure adventure = new Adventure();
+        adventure.initAdventure(yourPokemon);
     }
 }
